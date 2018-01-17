58de08fb9e806c8f2710202f8c849d91201e3cd3
adaildoneto <adaildo.neto@gmail.com>
2018-01-17 00:05:40 -0500

    mod especial

--- mod-especiais-teste.php
+++ mod-especiais-teste.php
@@ -1,22 +1,21 @@
 <div class="row">
 
 
+  <div class="col s12 no-padding">
+
+         <div class="col s6">
+
+        <h2><i class="icon-logo-agencia2017-01"></i> Especiais</h2>
+         </div>
+         <div class="col s6" style="padding-top: 30px;">
+         <a class="right grey-text text-darken-3" href="http://www.agencia.ac.gov.br/categoria/especiais/">Ver todos <i class="fa fa-long-arrow-right" style="font-size: 12px;" aria-hidden="true"></i>
+     </a>
+         </div>
 
 
-   <div class="col s12">
-     <div class="col s12 no-padding">
-
-            <div class="col s6">
-
-           <h2><i class="icon-logo-agencia2017-01"></i> Especiais</h2>
-            </div>
-            <div class="col s6" style="padding-top: 30px;">
-            <a class="right grey-text text-darken-3" href="http://www.agencia.ac.gov.br/categoria/especiais/">Ver todos <i class="fa fa-long-arrow-right" style="font-size: 12px;" aria-hidden="true"></i>
-        </a>
-            </div>
+  </div>
 
 
-     </div>
 
    <?php query_posts('showposts=4');?>
    <?php if (have_posts()): while (have_posts()) : the_post();?>
@@ -31,8 +30,6 @@
     <?php endwhile;endif;?>
     <div class="clearfix"></div>
 
-
-</div>
 
 
 
