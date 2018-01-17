eeb869c0104b92c4d4234e10a14d26aeaabc1516
adaildoneto <adaildo.neto@gmail.com>
2018-01-17 00:36:03 -0500

    fg

--- custom-teste.js
+++ custom-teste.js
@@ -389,11 +389,11 @@ jQuery(window).load(function() {
       jQuery(window).load(function() {
         var container = document.querySelector('#grid');
           var msnry = new Masonry( container, {
-            // use outer width of grid-sizer for columnWidth
+            // set positions with percent values
+    percentPosition: true,
     columnWidth: '.grid-sizer',
-    // do not use .grid-sizer in layout
-    itemSelector: '.grid-item',
-    percentPosition: true
+    itemSelector: '.grid-item'
+
 
             });
 
