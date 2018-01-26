cab849c133c4f1a597da75ad61f43b14dafc93eb
adaildoneto <adaildo.neto@gmail.com>
2018-01-26 16:19:13 -0500

    bugs

--- tempo-widget.php
+++ tempo-widget.php
@@ -1,12 +1,16 @@
 <div class="col s12 m4 l4 grid-item grid-width">
       <div class="card painel-noticias">
-        <div id="weather"></div>
+
         <?php
         $imgID = get_option ('odin_general');
         $imgMENU = wp_get_attachment_image_src( $imgID['imagemmenu'], 'slider-noticias');
                    ?>
         <div class="bloco-img-noticias2 especiais-img img-slider efeito" style="background: url('<?php echo $imgMENU[0]?>');">
 
+        </div>
+
+        <div class="bloco-des-noticias2">
+          <div id="weather"></div>
         </div>
 
 
