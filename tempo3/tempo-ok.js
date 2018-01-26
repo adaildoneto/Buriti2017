96037572df354a23cb13c3a02b01988d17b22028
adaildoneto <adaildo.neto@gmail.com>
2018-01-26 16:38:13 -0500

    bug

--- tempo3/tempo-ok.js
+++ tempo3/tempo-ok.js
@@ -31,8 +31,8 @@ $(document).ready(function() {
     woeid: '',
     unit: 'C',
     success: function(weather) {
-      html = '<h2><i class="icom-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<span class="white-text">'+weather.city+', '+weather.region+'</span>';
+      html = '<h2 class="white-text aligncenter"><i class="icom-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<span class="white-text aligncenter">'+weather.city+', '+weather.region+'</span>';
 
       $("#weather2").html(html);
     },
