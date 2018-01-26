37ec81b9585b8b12abaebc93766da278146f4bf6
adaildoneto <adaildo.neto@gmail.com>
2018-01-26 16:07:38 -0500

    bug

--- tempo-widget.php
+++ tempo-widget.php
@@ -1,17 +1,15 @@
-<div class="col s12 m8 l8 grid-item grid-width">
+<div class="col s12 m4 l4 grid-item grid-width">
       <div class="card painel-noticias">
-
-        <div class="background">
-          <?php
-          $imgID = get_option ('odin_general');
-          $imgMENU = wp_get_attachment_image_src( $imgID['imagemmenu'], 'slider-noticias');
-                     ?>
-          <img width="300" src="<?php echo $imgMENU[0]?>">
+        <?php
+        $imgID = get_option ('odin_general');
+        $imgMENU = wp_get_attachment_image_src( $imgID['imagemmenu'], 'slider-noticias');
+                   ?>
+        <div class="bloco-img-noticias2 especiais-img img-slider efeito" style="background: url('<?php echo $imgMENU[0]?>');">
 
         </div>
-<div id="weather"></div>
-      <div class="row" style="padding-top: 80px;">
 
-      </div>
+
+<div id="weather"></div>
+
       </div>
 </div>
