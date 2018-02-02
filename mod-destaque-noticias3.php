824ef52597e22d42d5323d3834dd322a33d2465f
adaildoneto <adaildo.neto@gmail.com>
2018-02-02 10:31:32 -0500

    bug

--- mod-destaque-noticias3.php
+++ mod-destaque-noticias3.php
@@ -3,6 +3,7 @@
 
       <?php
 
+
        $args = array (
          'pagination'             => false,
          'showposts'              => 5,
@@ -19,29 +20,29 @@
            $query->the_post();
 
            if ( $query->current_post == 0  ) {  // first post
-                if ( has_tag('destaque1') ) {
+
                       get_template_part ('destaque', 'grande');
-                }
+
            }
            if ( $query->current_post == 1  ) {  // first post
-                if ( has_tag('destaque2') ) {
+
                       get_template_part ('destaque', 'quadrado');
-                }
+
            }
            if ( $query->current_post == 2  ) {  // first post
-                if ( has_tag('destaque3') ) {
+
                       get_template_part ('normal', '');
-                }
+
            }
            if ( $query->current_post == 3  ) {  // first post
-                if ( has_tag('destaque4') ) {
+
                       get_template_part ('destaque', 'colorido');
-                }
+
            }
            if ( $query->current_post == 4 ) {  // first post
-                if ( has_tag('destaque5') ) {
+
                       get_template_part ('normal', '');
-                }
+              
            }
 
            wp_reset_postdata();
