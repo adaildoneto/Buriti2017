d4c1ecae14653dc1a1d8d209d71a19e29a572b50
adaildoneto <adaildo.neto@gmail.com>
2018-02-05 10:29:58 -0500

    horario

--- search.php
+++ search.php
@@ -18,10 +18,36 @@
 
                         <!-- Noticias 1 // Inicio do Loop -->
 
-                        <?php if (have_posts()): while (have_posts()) : the_post();
+                        <?php if (have_posts()): while (have_posts()) : the_post();?>
 
-                          get_template_part ('normal','');
-                        ?>
+                          <div class="col s12 m4 l4">
+                            <div class="card painel-noticias">
+                              <a href="<?php the_Permalink()?>" title="<?php the_title();?>" class="">
+
+                                  <div class="bloco-img-noticias3 especiais-img" style="background: url('<?php the_post_thumbnail_url( 'thumbnews' ); ?>');">
+
+                                </div></a>
+                                <div class="bloco-des-noticias3">
+                                    <span class="destaque-chamada">
+                                      <?php echo get_post_meta( $post->ID,'chamada', true )?>
+                                    </span>
+                                    <a href="<?php the_Permalink()?>" title="<?php the_title();?>" class="destaque-fonte orange-text darken-2">
+                                        <?php the_title();?>
+                                    </a>
+                                </div>
+                                <div class="nocanto2">
+                                  <span class="data-post white-text"> <?php the_time('d.m.Y');?> </span>
+                                  <span class="hora-post white-text"> <?php the_time('G:i');?></span></br>
+
+                                </div>
+                                <div class="nocanto4 tamanho-icones">
+                                    <?php include(TEMPLATEPATH.'/mod-social.php');?>
+                                </div>
+                            </div>
+                          </div>
+
+
+                        <?php endwhile;endif;?>
                         <div class="clearfix"></div>
 
 
