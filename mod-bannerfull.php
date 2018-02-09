867d29a0ebbb4cc6fac5fa8c7ab49ac67be25bce
adaildoneto <adaildo.neto@gmail.com>
2018-02-09 17:39:51 -0500

    update de carnaval

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -1,10 +1,10 @@
 <div class="row">
   <div class="parallax-container">
-      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/01/IGM_4774_mini.jpg"></div>
+      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2017/02/4346428182_d2cf1718c3_o.jpg"></div>
 <div style="margin-top:100px">
-  <h1 class="shadowcss grande white-text flow-text center-align">Monitoramento</br>dos Rios 2018</h1>
+  <h1 class="shadowcss grande white-text flow-text center-align">Carnaval 2018</h1>
   <div class="center-align">
-    <a class="waves-effect waves-light btn orange darken-5 " href="http://www.agencia.ac.gov.br/monitoramento-dos-rios/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
+    <a class="waves-effect waves-light btn orange darken-5 " href="http://www.agencia.ac.gov.br/carnava-2018/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
 
   </div>
 </div>
