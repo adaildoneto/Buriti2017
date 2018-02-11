e3ba3cac76d4219ce793e113b55eb65b7f1a6828
adaildoneto <adaildo.neto@gmail.com>
2018-02-10 19:31:56 -0500

    bug

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -4,7 +4,7 @@
 <div style="margin-top:100px">
   <h1 class="shadowcss grande white-text flow-text center-align">Carnaval 2018</h1>
   <div class="center-align">
-    <a class="waves-effect waves-light btn orange darken-5 " href="http://www.agencia.ac.gov.br/carnava-2018/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
+    <a class="waves-effect waves-light btn orange darken-5 " href="http://www.agencia.ac.gov.br/carnaval-2018/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
 
   </div>
 </div>
