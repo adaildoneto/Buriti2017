2868fec9f591890a27c51e8fcfe0453529b57d78
adaildoneto <adaildo.neto@gmail.com>
2018-02-16 11:13:41 -0500

    atualizando monitoramento dos rios

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -1,10 +1,10 @@
 <div class="row">
   <div class="parallax-container">
-      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2017/02/4346428182_d2cf1718c3_o.jpg"></div>
+      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/02/27908039_1576056919130113_7998319575579597525_o.jpg"></div>
 <div style="margin-top:100px">
-  <h1 class="shadowcss grande white-text flow-text center-align">Carnaval 2018</h1>
+  <h1 class="shadowcss grande white-text flow-text center-align">Monitoramento </br>dos Rios</h1>
   <div class="center-align">
-    <a class="waves-effect waves-light btn orange darken-5 " href="http://www.agencia.ac.gov.br/carnaval-2018/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
+    <a class="waves-effect waves-light btn orange darken-5 " href="http://www.agencia.ac.gov.br/monitoramento-dos-rios/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
 
   </div>
 </div>
