1f7955e6059ac4fca43507a4b17a9244f7d9dd20
adaildoneto <adaildo.neto@gmail.com>
2018-03-12 09:32:45 -0500

    upodate foto do rio

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -1,6 +1,6 @@
 <div class="row">
   <div class="parallax-container">
-      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/02/DSC_8606-min.jpg"></div>
+      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2016/04/24_02_16_imagem_do_dia_rio_muru_ARISON_JARDIM-8.jpg"></div>
 <div style="margin-top:100px">
   <h1 class="shadowcss grande white-text flow-text center-align">Monitoramento </br>dos Rios</h1>
   <div class="center-align">
