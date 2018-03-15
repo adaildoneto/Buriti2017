377d4ecd286c8fb8d7103cc53fa1935115dfd87c
adaildoneto <adaildo.neto@gmail.com>
2018-03-15 12:07:05 -0500

    banner nao aparece celular

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -1,11 +1,11 @@
-<div class="row">
+<div class="row show-on-large">
   <div class="parallax-container">
       <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2016/08/Secom_Acre_GM1208160700.jpg"></div>
 <div class="container" style="margin-top:200px">
   <div id="queroler" class="shadowcss grande white-text flow-text center-align">Assim é o mundo para quem não sabe ler
     </div>
   <div class="center-align">
-    <h4 class="white-text">Conheça o programa que vai </br>erradicar o analfabetismo no Acre.</h4>
+    <h4 class="white-text shadowcss">Conheça o programa que vai </br>erradicar o analfabetismo no Acre.</h4>
     <a class="waves-effect waves-light btn orange darken-5 " href="http://www.agencia.ac.gov.br/tag/quero-ler/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
 
   </div>
