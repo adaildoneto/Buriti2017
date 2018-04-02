cfeaebaced64cb3417fcc3261e55264cd2fd5541
adaildoneto <adaildo.neto@gmail.com>
2018-04-02 14:21:29 -0500

    bug erros dos universaitarios

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -2,7 +2,7 @@
   <div class="parallax-container">
       <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2016/11/quero_ler_Secom_Acre_SV_10-11-2016-11.jpg"></div>
 <div class="container" style="margin-top:200px">
-  <div id="queroler" class="shadowcss grande white-text flow-text center-align">Assim é o mundo para quem não sabe ler
+  <div id="queroler" class="shadowcss grande white-text flow-text center-align">Assim é o mundo para quem sabe ler
     </div>
   <div class="center-align">
     <h4 class="white-text shadowcss">Conheça o programa que vai </br>erradicar o analfabetismo no Acre.</h4>
