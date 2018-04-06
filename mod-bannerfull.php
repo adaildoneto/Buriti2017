5427e90a86436693633aabbc65a2e258246c7ed8
adaildoneto <adaildo.neto@gmail.com>
2018-04-06 10:04:18 -0500

    update

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -2,8 +2,10 @@
   <div class="parallax-container">
       <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2016/11/quero_ler_Secom_Acre_SV_10-11-2016-11.jpg"></div>
 <div class="container" style="margin-top:200px">
-  <div id="queroler" class="shadowcss grande white-text flow-text center-align">Assim é o mundo para quem sabe ler
-    </div>
+  <div id="queroler" class="shadowcss grande white-text flow-text center-align">Assim é o mundo para
+  </div></br>
+    <div id="queroler" class="shadowcss grande white-text flow-text center-align">quem sabe ler
+      </div>
   <div class="center-align">
     <h4 class="white-text shadowcss">Conheça o programa que vai </br>erradicar o analfabetismo no Acre.</h4>
     <a class="waves-effect waves-light btn orange darken-5 " href="http://www.agencia.ac.gov.br/tag/quero-ler/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
