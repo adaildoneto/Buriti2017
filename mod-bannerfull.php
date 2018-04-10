1e9ebb7082b1501123307552594c9e4185e88f71
adaildoneto <adaildo.neto@gmail.com>
2018-04-10 17:52:46 -0500

    policia

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -1,13 +1,13 @@
 w<div class="row show-on-large">
   <div class="parallax-container">
-      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2016/11/quero_ler_Secom_Acre_SV_10-11-2016-11.jpg"></div>
+      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/03/WhatsApp-Image-2018-03-15-at-19.35.59.jpeg"></div>
 <div class="container" style="margin-top:200px">
-  <div id="queroler" class="shadowcss grande white-text flow-text center-align">Assim é o mundo para
+  <div class="shadowcss grande white-text flow-text center-align">Homicídios têm redução
   </div></br>
-    <div id="queroler2" class="shadowcss grande white-text flow-text center-align">quem sabe ler
+    <div class="shadowcss grande white-text flow-text center-align">de 31,8% em março no Acre
       </div>
   <div class="center-align">
-    <h4 class="white-text shadowcss">Conheça o programa que vai </br>erradicar o analfabetismo no Acre.</h4>
+    <h4 class="white-text shadowcss">Veja o resultado positivo</h4>
     <a class="waves-effect waves-light btn orange darken-5 " href="http://www.agencia.ac.gov.br/tag/quero-ler/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
 
   </div>
