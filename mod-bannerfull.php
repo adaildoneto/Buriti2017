98301a03064f8d2083b9a5c9036877bddfa4442a
adaildoneto <adaildo.neto@gmail.com>
2018-04-10 18:23:05 -0500

    bug

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -2,7 +2,7 @@ w<div class="row show-on-large">
   <div class="parallax-container">
       <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/02/IMG-20170215-WA0146.jpg"></div>
 <div class="container" style="margin-top:200px">
-  <div class="shadowcss grande white-text flow-text center-align">Sete cidades não registram homicídios
+  <div class="shadowcss grande white-text flow-text center-align">7 cidades não registram homicídios
   </div></br>
     <div class="shadowcss grande white-text flow-text center-align">nos primeiros 90 dias de 2018
       </div>
