c0a974a2ac4589530b8e5764d11bf86699e24033
adaildoneto <adaildo.neto@gmail.com>
2018-04-10 18:17:30 -0500

    pronto corrigido

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -1,14 +1,14 @@
 w<div class="row show-on-large">
   <div class="parallax-container">
-      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/03/WhatsApp-Image-2018-03-15-at-19.35.59.jpeg"></div>
+      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/02/IMG-20170215-WA0146.jpg"></div>
 <div class="container" style="margin-top:200px">
-  <div class="shadowcss grande white-text flow-text center-align">Homicídios têm redução
+  <div class="shadowcss grande white-text flow-text center-align">Sete cidades não registram homicídios
   </div></br>
-    <div class="shadowcss grande white-text flow-text center-align">de 31,8% em março no Acre
+    <div class="shadowcss grande white-text flow-text center-align">nos primeiros 90 dias de 2018
       </div>
   <div class="center-align">
     <h4 class="white-text shadowcss">Veja o resultado positivo</h4>
-    <a class="waves-effect waves-light btn grey darken-5 " href=http://www.agencia.ac.gov.br/homicidios-tem-reducao-de-318-em-marco-no-acre/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
+    <a class="waves-effect waves-light btn grey darken-5 " href="http://www.agencia.ac.gov.br/sete-cidades-nao-registram-homicidios-nos-primeiros-90-dias-de-2018/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
 
   </div>
 </div>
