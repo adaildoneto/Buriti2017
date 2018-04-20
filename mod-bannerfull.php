d40f8f6fdce435006807a176270daf6428ef0efb
adaildoneto <adaildo.neto@gmail.com>
2018-04-20 17:43:52 -0500

    bbanner do indio

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -1,14 +1,11 @@
 w<div class="row show-on-large">
   <div class="parallax-container">
-      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/02/IMG-20170215-WA0146.jpg"></div>
+      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/04/fotos-indio.jpg"></div>
 <div class="container" style="margin-top:200px">
-  <div class="shadowcss grande white-text flow-text center-align">7 cidades não registram homicídios
-  </div></br>
-    <div class="shadowcss grande white-text flow-text center-align">nos primeiros 90 dias de 2018
+  <div class="shadowcss grande white-text flow-text center-align">Educação Indígena do Acre avança</br>valorizando os povos amazônicos
       </div>
   <div class="center-align">
-    <h4 class="white-text shadowcss">Veja o resultado positivo</h4>
-    <a class="waves-effect waves-light btn grey darken-5 " href="http://www.agencia.ac.gov.br/sete-cidades-nao-registram-homicidios-nos-primeiros-90-dias-de-2018/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
+    <a class="waves-effect waves-light btn grey darken-5 " href="http://www.agencia.ac.gov.br/educacao-indigena-do-acre-avanca-valorizando-os-povos-amazonicos/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
 
   </div>
 </div>
