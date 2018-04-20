10d53bac52086583c4a44c9ab15e30d07d613aca
adaildoneto <adaildo.neto@gmail.com>
2018-04-20 18:32:29 -0500

    foto

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -1,6 +1,6 @@
 <div class="row show-on-large">
   <div class="parallax-container">
-      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/04/fotos-indio.jpg"></div>
+      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2016/11/DSC_0299-1.jpg"></div>
 <div class="container" style="margin-top:200px">
   <div class="shadowcss grande white-text flow-text center-align">Educação Indígena do Acre avança valorizando os povos amazônicos
       </div>
