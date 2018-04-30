1f8bdda19794dc809e662ba42cd94522fa063ff7
adaildoneto <adaildo.neto@gmail.com>
2018-04-30 12:24:12 -0500

    banner

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -1,11 +1,11 @@
 <div class="row show-on-large">
   <div class="parallax-container">
-      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2016/11/DSC_0299-1.jpg"></div>
+      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/04/DJI_0064-2.jpeg"></div>
 <div class="container" style="margin-top:200px">
-  <div class="shadowcss grande white-text flow-text center-align">Educação Indígena do Acre avança valorizando os povos amazônicos
+  <div class="shadowcss grande white-text flow-text center-align">Com Florestas Plantadas, governo promove geração de renda e conservação
       </div>
   <div class="center-align">
-    <a class="waves-effect waves-light btn grey darken-5 " href="http://www.agencia.ac.gov.br/educacao-indigena-do-acre-avanca-valorizando-os-povos-amazonicos/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
+    <a class="waves-effect waves-light btn grey darken-5 " href="http://www.agencia.ac.gov.br/com-florestas-plantadas-governo-promove-geracao-de-renda-e-conservacao/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
 
   </div>
 </div>
