87653facb42cdc969f31268027fad80a54c3efa3
adaildoneto <adaildo.neto@gmail.com>
2018-05-02 13:54:31 -0500

    bug

--- sidebar.php
+++ sidebar.php
@@ -98,7 +98,7 @@
 
 <!--  chamando widget publicidade lateral -->
 <div >
-  <div id="slick" data-slick='{"slidesToShow": 1, "slidesToScroll": 1, "dots":true, "arrows":false}'>
+  <div id="slick" data-slick='{"slidesToShow": 1, "slidesToScroll": 1, "dots":true, "arrows":false, "autoplay":true}'>
     <div>
   <?php dynamic_sidebar( 'publicidade-lateral' ); ?>
     </div>
