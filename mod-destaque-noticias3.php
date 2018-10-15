6bfa5037ab0f7e1de670a37f8d2174cd11169ed8
adaildoneto <adaildo.neto@gmail.com>
2018-10-15 10:33:45 -0500

    error no destaque

--- mod-destaque-noticias3.php
+++ mod-destaque-noticias3.php
@@ -13,12 +13,10 @@
    <div id="test1" class="col s12">
 
       <?php
-
-
     $args = array (
       'pagination'             => false,
-      'showposts'              => 5,
-      'tag'                    => array ('destaque1, destaque2, destaque3, destaque4, destaque5'),
+      'showposts'              => 1,
+      'tag'                    => array ('destaque1'),
       'ignore_sticky_posts'    => true,
 
     );
@@ -31,35 +29,113 @@
         $query->the_post();
 
         if ( $query->current_post == 0  ) {  // first post
-
                    get_template_part ('destaque', 'grande');
-
-        }
-        if ( $query->current_post == 1  ) {  // first post
-
-                   get_template_part ('destaque', 'quadrado');
-
-        }
-        if ( $query->current_post == 2  ) {  // first post
-
-                   get_template_part ('destaque', 'colorido');
-
-        }
-        if ( $query->current_post == 3  ) {  // first post
-
-                   get_template_part ('destaque', 'quadrado');
-
-        }
-        if ( $query->current_post == 4 ) {  // first post
-
-                   get_template_part ('destaque', 'colorido');
-
         }
 
         wp_reset_postdata();
 
       }
     ?>
+
+    <?php
+  $args = array (
+    'pagination'             => false,
+    'showposts'              => 1,
+    'tag'                    => array ('destaque2'),
+    'ignore_sticky_posts'    => true,
+
+  );
+  // The Query
+  $query = new WP_Query( $args );
+
+  // The Loop
+  while ( $query->have_posts() ) {
+
+      $query->the_post();
+
+      if ( $query->current_post == 0  ) {  // first post
+                 get_template_part ('destaque', 'quadrado');
+      }
+
+      wp_reset_postdata();
+
+    }
+  ?>
+
+  <?php
+$args = array (
+  'pagination'             => false,
+  'showposts'              => 1,
+  'tag'                    => array ('destaque3'),
+  'ignore_sticky_posts'    => true,
+
+);
+// The Query
+$query = new WP_Query( $args );
+
+// The Loop
+while ( $query->have_posts() ) {
+
+    $query->the_post();
+
+    if ( $query->current_post == 0  ) {  // first post
+               get_template_part ('destaque', 'colorido');
+    }
+
+    wp_reset_postdata();
+
+  }
+?>
+
+<?php
+$args = array (
+'pagination'             => false,
+'showposts'              => 1,
+'tag'                    => array ('destaque4'),
+'ignore_sticky_posts'    => true,
+
+);
+// The Query
+$query = new WP_Query( $args );
+
+// The Loop
+while ( $query->have_posts() ) {
+
+  $query->the_post();
+
+  if ( $query->current_post == 0  ) {  // first post
+             get_template_part ('destaque', 'quadrado');
+  }
+
+  wp_reset_postdata();
+
+}
+?>
+
+<?php
+$args = array (
+'pagination'             => false,
+'showposts'              => 1,
+'tag'                    => array ('destaque5'),
+'ignore_sticky_posts'    => true,
+
+);
+// The Query
+$query = new WP_Query( $args );
+
+// The Loop
+while ( $query->have_posts() ) {
+
+  $query->the_post();
+
+  if ( $query->current_post == 0  ) {  // first post
+             get_template_part ('destaque', 'colorido');  
+  }
+
+  wp_reset_postdata();
+
+}
+?>
 
   </div>
 
