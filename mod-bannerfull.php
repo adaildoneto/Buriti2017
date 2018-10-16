ca94dc70f2ed71f3484564f496bca842cd4629c9
adaildoneto <adaildo.neto@gmail.com>
2018-10-16 17:18:46 -0500

    colocando banner full

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -1,17 +1,15 @@
-<!--
-
 <div class="row show-on-large">
   <div class="parallax-container">
-      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/04/DJI_0064-2.jpeg"></div>
+      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2017/09/castanha_pedro_devani-3.jpg"></div>
 <div class="container" style="margin-top:200px">
-  <div class="shadowcss grande white-text flow-text center-align">Com Florestas Plantadas, governo promove geração de renda e conservação
+  <div class="shadowcss grande white-text flow-text center-align">#NovoAcre
       </div>
   <div class="center-align">
-    <a class="waves-effect waves-light btn orange darken-5 " href="http://www.agencia.ac.gov.br/com-florestas-plantadas-governo-promove-geracao-de-renda-e-conservacao/"><i class="material-icons left">arrow_drop_down_circle</i>Acesse agora!</a>
+    <a class="waves-effect waves-light btn orange darken-5 " href="#"><i class="material-icons left">arrow_drop_down_circle</i>Suas histórias, nossa identidade</a>
 
   </div>
 </div>
 
     </div>
 
-</div> -->
+</div>
