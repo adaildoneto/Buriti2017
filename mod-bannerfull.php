490d96c209e76fd9195469040ac8c4b7e9a4c32a
adaildoneto <adaildo.neto@gmail.com>
2018-10-16 17:21:05 -0500

    bug

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -2,10 +2,10 @@
   <div class="parallax-container">
       <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2017/09/castanha_pedro_devani-3.jpg"></div>
 <div class="container" style="margin-top:200px">
-  <div class="shadowcss grande white-text flow-text center-align">#NovoAcre
+  <div class="shadowcss grande white-text flow-text center-align">Suas histórias, nossa identidade
       </div>
   <div class="center-align">
-    <a class="waves-effect waves-light btn orange darken-5 " href="#"><i class="material-icons left">arrow_drop_down_circle</i>Suas histórias, nossa identidade</a>
+    <a class="waves-effect waves-light btn orange darken-5 " href="#"><i class="material-icons left">arrow_drop_down_circle</i>Em breve...</a>
 
   </div>
 </div>
