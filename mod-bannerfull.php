9db0933c3da3002ad16d8257a2ae23d77f82ff00
adaildoneto <adaildo.neto@gmail.com>
2018-10-17 09:58:20 -0500

    colocando o banner no topo

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -1,8 +1,8 @@
 <div class="row show-on-large">
   <div class="parallax-container">
-      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2017/09/castanha_pedro_devani-3.jpg"></div>
+      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2017/03/Croa-Gleilson-Miranda.jpg"></div>
 <div class="container" style="margin-top:200px">
-  <div class="shadowcss grande white-text flow-text center-align">Suas histórias, nossa identidade
+  <div class="shadowcss grande white-text flow-text center-align">SUAS HISTÓRIAS,</BR> NOSSA IDENTIDADE
       </div>
   <div class="center-align">
     <a class="waves-effect waves-light btn orange darken-5 " href="#"><i class="material-icons left">arrow_drop_down_circle</i>Em breve...</a>
