c7fbf88ca0e23b67624dfa01da587cb5226011c2
adaildoneto <adaildo.neto@gmail.com>
2018-10-18 14:29:01 -0500

    sbug

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -1,6 +1,6 @@
 <div class="row show-on-large">
   <div class="parallax-container">
-      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2014/12/produ%C3%A7%C3%A3o_juru%C3%A1_arison_jardim-21.jpg"></div>
+      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2017/04/NOVO_SERINGAL_ARISON-4.jpg"></div>
 <div class="container" style="margin-top:200px">
   <div class="shadowcss grande white-text flow-text center-align">SUAS HISTÓRIAS,</BR> NOSSA IDENTIDADE
       </div>
