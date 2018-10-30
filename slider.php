6f7f3af7f459d4e1b957bfbe8f579d74ed43efc5
adaildoneto <adaildo.neto@gmail.com>
2018-10-30 12:58:03 -0500

    bug slider

--- slider.php
+++ slider.php
@@ -4,7 +4,16 @@
   <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider-noticias2' ); ?>
     <div class="slider-noticias img-slider" style="background: url('<?php echo $image[0]; ?>');">
       <div class="bloco-slider-noticias">
-        <h2><span class="line-text orange darken-2"></span><?php the_title();?></h2>
+        <h2><span class="line-text orange darken-2"></span><?php
+        $tituloPost = get_the_title();
+        $tituloCapa = get_post_meta( $post->ID,'titulo-capa', true );
+        if(empty($tituloCapa)){
+           $titulo = $tituloPost;
+        }else{
+          $titulo = $tituloCapa;
+        }
+        echo $titulo;
+        ?></h2>
       </div>
     </div>
   </a>
