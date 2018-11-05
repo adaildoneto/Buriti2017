fe2a2f971916ee58c0ad9f8a61bca89e4bcc498b
adaildoneto <adaildo.neto@gmail.com>
2018-11-05 09:55:09 -0500

    ok - banner full

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -1,11 +1,11 @@
 <div class="row show-on-large">
   <div class="parallax-container">
-      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2017/04/NOVO_SERINGAL_ARISON-4.jpg"></div>
+      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/10/AH3K3336-min-1800x1200.jpg"></div>
 <div class="container" style="margin-top:200px">
-  <div class="shadowcss grande white-text flow-text center-align">SUAS HISTÓRIAS,</BR> NOSSA IDENTIDADE
+  <div class="shadowcss grande white-text flow-text center-align">Conheça o Parque Nacional da Serra do Divisor,</br> verdadeiro santuário ecológico
       </div>
   <div class="center-align">
-    <a class="waves-effect waves-light btn orange darken-5 " href="#"><i class="material-icons left">arrow_drop_down_circle</i>Em breve...</a>
+    <a class="waves-effect waves-light btn orange darken-5 " href="http://www.agencia.ac.gov.br/conheca-o-parque-nacional-da-serra-do-divisor-verdadeiro-santuario-ecologico"><i class="material-icons left">arrow_drop_down_circle</i>Em breve...</a>
 
   </div>
 </div>
