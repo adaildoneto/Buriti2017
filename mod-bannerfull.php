04ff039e0ec37c7f9889d82a531c5a8876a43114
adaildoneto <adaildo.neto@gmail.com>
2018-11-05 09:58:21 -0500

    BUG

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -2,7 +2,7 @@
   <div class="parallax-container">
       <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/10/AH3K3336-min-1800x1200.jpg"></div>
 <div class="container" style="margin-top:200px">
-  <div class="shadowcss grande white-text flow-text center-align">Conheça o Parque Nacional da Serra do Divisor,</br> verdadeiro santuário ecológico
+  <div class="shadowcss grande white-text flow-text center-align">Conheça o Parque Nacional</BR> da Serra do Divisor
       </div>
   <div class="center-align">
     <a class="waves-effect waves-light btn orange darken-5 " href="http://www.agencia.ac.gov.br/conheca-o-parque-nacional-da-serra-do-divisor-verdadeiro-santuario-ecologico"><i class="material-icons left">arrow_drop_down_circle</i>Em breve...</a>
