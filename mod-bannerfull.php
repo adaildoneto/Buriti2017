e0e6ae36ac56e20c04729d139620d76ba89ea877
adaildoneto <adaildo.neto@gmail.com>
2018-11-09 11:37:06 -0500

    update

--- mod-bannerfull.php
+++ mod-bannerfull.php
@@ -1,11 +1,83 @@
-<div class="row show-on-large">
+
   <div class="parallax-container">
-      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/10/AH3K3336-min-1800x1200.jpg"></div>
-<div class="container" style="margin-top:200px">
+      <div class="parallax"><img src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/11/AH3K2917.jpg"></div>
+<div class="container" style="margin-top:60px">
+
   <div class="shadowcss grande white-text flow-text center-align"><img class="responsive-img" src="http://www.agencia.ac.gov.br/wp-content/uploads/2018/11/PRONTOPARAOFUTURO.png" alt="Pronto para o futuro">
       </div>
+
   <div class="center-align">
-    <a class="waves-effect waves-light btn orange darken-5 " href="http://www.agencia.ac.gov.br/conheca-o-parque-nacional-da-serra-do-divisor-verdadeiro-santuario-ecologico"><i class="material-icons left">arrow_drop_down_circle</i>Acesse aqui!</a>
+   <div class="row hide-on-med-and-down" >
+     <?php
+
+      $myargs = array (
+        'pagination'             => true,
+        'tag'							       => array ('prontoparaofuturo'),
+        'posts_per_page'         => 3,
+        'ignore_sticky_posts'    => true,
+
+      );
+      // The Query
+      $myquery = new WP_Query( $myargs );
+
+      // The Loop
+      while ( $myquery->have_posts() ) {
+
+          $myquery->the_post();
+
+
+
+          get_template_part( 'destaque', 'quadrado' );
+
+
+                wp_reset_postdata();
+
+        }
+
+      ?>
+
+
+  </div>
+  <div class="row hide-on-med-and-up" >
+    <?php
+
+     $myargs = array (
+       'pagination'             => true,
+       'tag'							       => array ('prontoparaofuturo'),
+       'posts_per_page'         => 1,
+       'ignore_sticky_posts'    => true,
+
+     );
+     // The Query
+     $myquery = new WP_Query( $myargs );
+
+     // The Loop
+     while ( $myquery->have_posts() ) {
+
+         $myquery->the_post();
+
+
+
+         get_template_part( 'destaque', 'quadrado' );
+
+
+               wp_reset_postdata();
+
+       }
+
+     ?>
+
+
+ </div>
+
+  <div class="row no-padding">
+
+         <div class="col s6"> </div>
+         <div class="col s6" style="padding-top: -20px;">
+         <a class="right white-text" href="http://www.agencia.ac.gov.br/categoria/no-gabinete/">Ver todos <i class="fa fa-long-arrow-right" style="font-size: 12px;" aria-hidden="true"></i>
+     </a>
+         </div>
+
 
   </div>
 </div>
